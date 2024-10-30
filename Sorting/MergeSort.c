2704deb63e8cf024010a3c1eef79f01c18737e41
Ankan Chatterjee <ankanchatterjee4855@gmail.com>
2024-10-30 16:36:56 +0530

    modified:   Sorting/MergeSort.c 	new file:   Sorting/MergeSort.exe

--- Sorting/MergeSort.c
+++ Sorting/MergeSort.c
@@ -1,38 +1,110 @@
-#include<stdio.h>
+#include <stdio.h>
 
 // Merging in one array from two arrays
 
-void merge(int A[], int B[], int C[], int m, int n){
+void merge(int A[], int B[], int C[], int m, int n)
+{
     int i, j, k;
-    i=j=k=0;
-    while(i<m && j<n){
+    i = j = k = 0;
+    while (i < m && j < n)
+    {
         if (A[i] <= B[j])
         {
             C[k] = A[i];
-            i++; k++;
-        } else{
+            i++;
+            k++;
+        }
+        else
+        {
             C[k] = B[j];
-            j++; k++;
-        }        
+            j++;
+            k++;
+        }
     }
-    while(i<m){
+    while (i < m)
+    {
         C[k] = A[i];
-        i++; k++;
+        i++;
+        k++;
     }
-    while(j<n){
+    while (j < n)
+    {
         C[k] = B[j];
-        j++; k++;
+        j++;
+        k++;
     }
-
 }
 
 // merging in a single array
 
-void singleMerge(){
+void singleMerge(int A[], int mid, int low, int high)
+{
+    int i, j, k, B[100];
+    i = low;
+    j = mid + 1;
+    k = low;
+    while (i <= mid && j <= high)
+    {
+        if (A[i] < A[j])
+        {
+            B[k] = A[i];
+            i++; k++;
+        }
+        else
+        {
+            B[k] = A[j];
+            j++; k++;
+        }
+    }
+    while (i <= mid)
+    {
+        B[k] = A[i];
+        i++; k++;
+    }
+    while (j <= high)
+    {
+        B[k] = A[j];
+        j++; k++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        A[i] = B[i];
+    }
     
 }
 
-int main(){
+void MS(int A[], int low, int high){
+    int mid;
+    if (low < high) {
+        mid = (low + high) / 2;
+        MS(A, low, mid);
+        MS(A, mid + 1, high);
+        singleMerge (A, mid, low, high);
+    }
+}
 
+void printArray(int *A, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int A[] = {9, 14, 4, 8, 7, 5, 6};
+    int n = 7;
+
+    printf("Array before sorting: ");
+    printArray(A, n);
+
+    MS(A, 0, 6);
+
+    printf("Array after sorting: ");
+    printArray(A, n);
+
+    return 0;
     return 0;
 }
